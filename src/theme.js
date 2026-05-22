06d6fddbd6c3ffb0e1ebdabb835c19ae67d2560a
vanya <ivanbrynzei05@gmail.com>
2026-05-23 01:00:01 +0300

    init

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+    token: {
+        colorPrimary: "#e53935",
+    },
+    components: {
+        Button: {
+            borderRadius: 0,
+            borderRadiusLG: 0,
+            borderRadiusSM: 0,
+        },
+    },
+};
